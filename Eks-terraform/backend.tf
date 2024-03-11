5404be841907fa31817d2e0283845a144dfb9404
Rohit Bhosale <149032407+rohit808077@users.noreply.github.com>
2024-03-11 15:52:16 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "new-bucket-8080" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-southeast-2"
   }
